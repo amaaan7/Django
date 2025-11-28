dcd20d577861063abca8cff1325fe99dc2e06a19
amaan <amanmulla231@gmail.com>
2025-11-28 12:26:48 +0530

    testing

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Auguest 27, 2018'
+
+    },
+        
+    {
+        'author': 'Amaan',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Auguest 28, 2018'
+
+    }
+]
 
 def home(request):
-    return HttpResponse('Blog Home Page')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('Blog About')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
